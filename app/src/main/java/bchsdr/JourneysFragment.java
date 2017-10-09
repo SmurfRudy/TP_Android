40c4bf6b4dd7ec99a24e2f986e3e48cd5ec84048
MaximeCollot <maxime.collot.69@gmail.com>
2017-10-09 15:12:23 +0200

    Init app

---
+++ app/src/main/java/bchsdr/JourneysFragment.java
@@ -0,0 +1,51 @@
+package bchsdr;
+
+import android.app.Fragment;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import bchsdr.adapter.JourneyListAdapter;
+import bchsdr.model.Journey;
+import bchsdr.tp_android_1.R;
+import bchsdr.tp_android_1.databinding.JourneysFragmentBinding;
+
+/**
+ * Created by Maxime on 09/10/2017.
+ */
+
+public class JourneysFragment extends Fragment {
+    List<Journey> journeys = new ArrayList<Journey>();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
+        initList();
+        JourneysFragmentBinding binding = DataBindingUtil.inflate(inflater, R.layout.journeys_fragment, container, false);
+        binding.journeysList.setLayoutManager(new LinearLayoutManager(binding.getRoot().getContext()));
+        binding.journeysList.setAdapter(new JourneyListAdapter(journeys));
+        return binding.getRoot();
+    }
+
+    private void initList() {
+        Journey copenhagen = new Journey();
+        copenhagen.setName("Copenhagen");
+        this.journeys.add(copenhagen);
+
+        Journey dublin = new Journey();
+        dublin.setName("Dublin");
+        this.journeys.add(dublin);
+
+        Journey prague = new Journey();
+        prague.setName("Prague");
+        this.journeys.add(prague);
+    }
+}
---
+++ app/src/main/java/bchsdr/model/Journey.java
@@ -0,0 +1,49 @@
+package bchsdr.model;
+
+import java.util.Calendar;
+
+/**
+ * Created by Maxime on 09/10/2017.
+ */
+
+public class Journey {
+    private String name;
+    private Calendar from;
+    private Calendar to;
+
+    public Journey(){
+        name="";
+        from = Calendar.getInstance();
+        to = Calendar.getInstance();
+    }
+
+    public Journey (String name, Calendar from, Calendar to) {
+        this.name = name;
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Calendar getFrom() {
+        return from;
+    }
+
+    public void setFrom(Calendar from) {
+        this.from = from;
+    }
+
+    public Calendar getTo() {
+        return to;
+    }
+
+    public void setTo(Calendar to) {
+        this.to = to;
+    }
+}
